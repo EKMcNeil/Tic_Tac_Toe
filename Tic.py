4e4c258b00a1f69c2c2f36f9e98c6bec2ed6f5d6
EC2 Default User <ec2-user@ip-172-31-39-33.us-east-2.compute.internal>
2019-04-26 20:58:39 +0000

    Almost at Tutorial 3 with Indexes. Printing game layout correctly

---
+++ Tic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
+
+print(" 0  1  2")
+
+count = 0
+
+for count, row in enumerate(game):
+    print(count, row)
+    
+mark = range(0:5)
+
