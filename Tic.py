5c11033c03b1cdd3d5d17889fedd2a0303770cde
EC2 Default User <ec2-user@ip-172-31-39-33.us-east-2.compute.internal>
2019-05-07 19:29:39 +0000

    Input from player defined in edited function. No use of sys module

--- Tic.py
+++ Tic.py
@@ -4,20 +4,24 @@ game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
 
-def board():
+def board(move = 0, row = 0, column = 0, display = False):
+    if not display:
+        game[row][column] = move
     print("   0  1  2")
     for count, row in enumerate(game):
         print(count, row)
     print(board)
 
-board()
+board(display = True)
+board(move = 1, row = 1, column = 2)
 
-import sys
 
-if len(sys.argv) != 1:
-    print("Incorrect Number of Arguments")
-    sys.exit(1)
+#import sys
 
-move = sys.argv
+#if len(sys.argv) != 1:
+#    print("Incorrect Number of Arguments")
+#    sys.exit(1)
 
-if move = 
+#move = sys.argv
+
+#if move = 
